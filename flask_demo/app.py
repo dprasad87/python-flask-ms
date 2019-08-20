c317efc8e53d970c4288bcac4fc6081851698fec
dprasad87 <dwarika.prasad87@gmail.com>
2019-08-20 12:53:56 +0530

    Introduced Blueprint and
    Fixed ModuleNotFoundError: No module named 'flask_demo' by changing the import starting from api

--- flask_demo/app.py
+++ flask_demo/app.py
@@ -1,7 +1,6 @@
 import logging
 
-from flask import Flask
-from flask_restplus import Api, Resource
+from flask import Flask, Blueprint
 from api.restplus import api
 
 
@@ -9,16 +8,14 @@ log = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
-# api = Api(app)
-
-
-@api.route("/hello")
-class HelloWorld(Resource):
-    def get(self):
-        return {"hello": "world"}
+def initialize_app(flask_app):
+    blueprint = Blueprint('api', __name__, url_prefix="/api")
+    api.init_app(blueprint)
+    flask_app.register_blueprint(blueprint)
 
 
 if __name__ == '__main__':
+    initialize_app(app)
     log.info(">>> Starting the dev app at url http://{}/".format('localhost'))
     log.info(">>> App Name: %s " % app.name)
     app.run('localhost', '8081', debug=True)
