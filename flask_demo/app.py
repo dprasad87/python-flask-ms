db4bebd42dab975557f5d9b052498421bdcb383e
dprasad87 <dwarika.prasad87@gmail.com>
2019-08-20 11:29:17 +0530

    Initial working api

---
+++ flask_demo/app.py
@@ -0,0 +1,19 @@
+import logging
+
+from flask import Flask
+from flask_restplus import Api, Resource
+
+log = logging.getLogger(__name__)
+app = Flask(__name__)
+
+
+@api.route("/hello")
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "world"}
+
+
+if __name__ == '__main__':
+    log.info(">>> Starting the dev app at url http://{}/".format('localhost'))
+    log.info(">>> App Name: %s " % app.name)
+    app.run('localhost', '8081', debug=True)
